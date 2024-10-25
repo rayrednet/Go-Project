65fa727bf9459efa56e411259a66adb13bdf7eff
Rayssa Ravelia <rayssa.sibarani@gmail.com>
2024-10-25 14:40:18 +0700

    maps in go

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
+
 import (
-  "fmt"
-  "strings"
-  "Go-Project/helper"
+	"Go-Project/helper"
+	"fmt"
+	"strconv"
 )
 
 const conferenceTickets uint = 50
 var conferenceName  = "Go Conference"
 var remainingTickets uint = 50
-var bookings = [] string{}
+var bookings = make([]map[string]string, 0)
 
 func main() {
   greetUsers()
@@ -51,8 +52,7 @@ func main() {
   func getFirstNames() []string {
     firstNames := []string{}
         for _, booking := range bookings {
-          var names = strings.Fields(booking)
-          firstNames = append(firstNames, names[0])
+          firstNames = append(firstNames, booking["firstName"])
         }
         return firstNames
   }
@@ -80,7 +80,15 @@ func main() {
 
   func bookTickets (userTickets uint, firstName string, lastName string, email string) {
     remainingTickets -= userTickets
-    bookings = append(bookings, firstName + " " + lastName)
+
+    var userData = make(map[string]string)
+    userData["firstName"] = firstName
+    userData["lastName"] = lastName
+    userData["email"] = email
+    userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+
+    bookings = append(bookings, userData)
+    fmt.Printf("List of bookings: %v\n", bookings)
 
     fmt.Printf ("Thank you %v %v for booking %v tickets for %v. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, conferenceName, email)
     fmt.Printf("There are %v tickets remaining for %v\n", remainingTickets, conferenceName)
